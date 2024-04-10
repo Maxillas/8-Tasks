bd65515470718d4d57fd44c6ca5fccf20646270c
Maxillas <maksanchutin@mail.ru>
2024-04-10 20:14:33 +0500

    ref: task 7

--- 7Task/Task_7.py
+++ 7Task/Task_7.py
@@ -1,21 +1,13 @@
 import unittest
 
 def secondMaximum(inputList):
-    if len(inputList) == 1:
-        return inputList[0]
-    if len(inputList) == 0:
-        return
     def maximumSearch(index, firstMax, secondMax):
-        if index >= len(inputList):
+        if index == len(inputList):
             return secondMax
         if inputList[index] >= firstMax:
             secondMax, firstMax = firstMax, inputList[index]
-        elif inputList[index] >= secondMax:
+        elif inputList[index] > secondMax:
             secondMax = inputList[index]
         index += 1
         return maximumSearch(index, firstMax, secondMax)
-
-    if (inputList[0] >= inputList[1]):
-        return maximumSearch(2, inputList[0], inputList[1])
-    elif (inputList[0] <= inputList[1]):
-        return maximumSearch(2, inputList[1], inputList[0])
+    return maximumSearch(0, inputList[0], inputList[0])
