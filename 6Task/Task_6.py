4d6a33ae8a8db2afd5dd4081b816f0c50dfa3aaf
Maxillas <maksanchutin@mail.ru>
2024-04-10 18:40:48 +0500

    ref: task 6

--- 6Task/Task_6.py
+++ 6Task/Task_6.py
@@ -2,8 +2,7 @@ def printEvenIndex(inputList):
     def indexPrint(inputIndex):
         if inputIndex > len(inputList) - 1:
             return
-        if inputIndex % 2 == 0:
-            print(inputList[inputIndex])
-        inputIndex += 1
+        print(inputList[inputIndex])
+        inputIndex += 2
         indexPrint(inputIndex)
     indexPrint(0)
